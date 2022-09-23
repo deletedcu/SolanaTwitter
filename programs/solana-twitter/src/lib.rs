1cbc265991448114f5df04841b59e8cf7374e699
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-09-23 11:58:18 +0900

    update program ID

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("2eyJNpUiqaLghKUctM91UQET66Yx8BVbds2LvVvjd1Cg");
 
 #[program]
 pub mod solana_twitter {
