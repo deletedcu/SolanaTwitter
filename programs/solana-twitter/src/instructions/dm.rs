65a424d8a6bb9cd5362bae6a63d2fd530a6440e6
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-09-26 12:36:32 +0900

    implemented dm logic

---
+++ programs/solana-twitter/src/instructions/dm.rs
@@ -0,0 +1,42 @@
+use crate::errors::*;
+use crate::state::dm::*;
+use anchor_lang::*;
+
+pub fn send_dm(ctx: Context<SendDm>, recipient: Pubkey, content: String) -> Result<()> {
+  let dm = &mut ctx.accounts.dm;
+  let user: &Signer = &ctx.accounts.user;
+  let clock: Clock = Clock::get().unwrap();
+
+  require!(content.chars().count() <= 280, ErrorCode::TooLong);
+  require!(content.chars().count() > 0, ErrorCode::NoContent);
+
+  dm.user = *user.key;
+  dm.recipient = recipient;
+  dm.timestamp = clock.unix_timestamp;
+  dm.content = content;
+  dm.edited = false;
+
+  Ok(())
+}
+
+pub fn update_dm(ctx: Context<UpdateDm>, new_content: String) -> Result<()> {
+  let dm = &mut ctx.accounts.dm;
+
+  require!(dm.content != new_content, ErrorCode::NothingChanged);
+  require!(content.chars().count() <= 280, ErrorCode::TooLong);
+  require!(content.chars().count() > 0, ErrorCode::NoContent);
+
+  dm.content = new_content;
+  dm.edited = true;
+
+  Ok(())
+}
+
+pub fn delete_dm(ctx: Context<DeleteDm>) -> Result<()> {
+  let dm = &mut ctx.accounts.dm;
+
+  dm.content = "".to_string();
+  dm.edited = true;
+
+  Ok(())
+}
