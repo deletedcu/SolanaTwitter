f385931f7d899a7b1ffe13869e2889d61553ecf2
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-10-03 11:15:57 +0900

    add profile page

---
+++ app/pages/profile/index.tsx
@@ -0,0 +1,35 @@
+import { useAnchorWallet } from "@solana/wallet-adapter-react";
+import { useEffect, useState } from "react";
+import TweetForm from "../../components/TweetForm";
+import TweetList from "../../components/TweetList";
+import { Tweet } from "../../models";
+import Base from "../../templates/Base";
+import { fetchTweets, userFilter } from "../api/tweets";
+
+export default function Profile() {
+  const [tweets, setTweets] = useState<Tweet[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const wallet = useAnchorWallet();
+
+  const addTweet = (tweet: Tweet) => setTweets([...tweets, tweet]);
+
+  useEffect(() => {
+    if (!wallet) return;
+    fetchTweets([userFilter(wallet.publicKey.toBase58())])
+      .then((fetchedTweets) => setTweets(fetchedTweets))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <Base>
+      {wallet && (
+        <div className="border-b bg-gray-50 px-8 py-4">
+          {wallet.publicKey.toBase58()}
+        </div>
+      )}
+      <TweetForm added={addTweet} />
+      <TweetList tweets={tweets} loading={loading} />
+    </Base>
+  );
+}
