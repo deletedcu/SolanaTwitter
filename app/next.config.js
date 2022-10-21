f67e6c41e24001f9d732b3b9cfe6950672f514af
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-10-21 23:00:49 +0900

    Fix page not found issue

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true,
   images: {
     domains: ["avatars.dicebear.com"],
     unoptimized: true,
--- app/src/contexts/index.tsx
+++ app/src/contexts/index.tsx
@@ -1,10 +1,15 @@
+import dynamic from "next/dynamic";
 import { ReactNode } from "react";
 import { CommentsProvider } from "./CommentsContext";
-import { SolanaProvider } from "./SolanaContext";
 import { TagsProvider } from "./TagsContext";
 import { ThemeProvider } from "./ThemeContext";
 import { TweetsProvider } from "./TweetsContext";
 import { UsersProvider } from "./UsersContext";
+
+const SolanaProvider = dynamic(
+  () => import('./SolanaContext').then(({ SolanaProvider }) => SolanaProvider),
+  { ssr: false },
+);
 
 export default function AppContext({ children }: { children: ReactNode }) {
   return (
